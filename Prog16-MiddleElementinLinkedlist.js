be9e6bb095d0d0bd51b236f4dc6f77d94b0d70b9
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-01-24 16:56:43 +0530

    middle element update

--- Prog16-MiddleElementinLinkedlist.js
+++ Prog16-MiddleElementinLinkedlist.js
@@ -5,13 +5,19 @@ class Node{
     }
 }
 
-let head = new Node(10)
-let scecond = new Node(20)
-let third = new Node(30)
-let forth = new Node(40)
-let fifth = new Node(50)
+let head = new Node(10);
+let scecond = new Node(20);
+let third = new Node(30);
+let forth = new Node(40);
+let fifth = new Node(50);
 
 head.next = second;
 second.next = third;
 third.next = forth;
 forth.next = fifth;
+fifth.next = null;
+
+function getMiddleNode(head){
+   let fastptr = head;
+   let slowptr = head;
+}
