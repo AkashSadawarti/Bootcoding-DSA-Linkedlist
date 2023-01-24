838b4bb1b5397d5981c609c6f27147338c977087
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-01-24 16:53:15 +0530

    middle element

---
+++ Prog16-MiddleElementinLinkedlist.js
@@ -0,0 +1,12 @@
+class Node{
+    constructor(data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+let head = new Node(10)
+let scecond = new Node(20)
+let third = new Node(30)
+let forth = new Node(40)
+let fifth = new Node(50)
