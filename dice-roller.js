eb0a0ad4967d2bbf30c8a397ce8f32b922daa402
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-06-01 15:09:40 +0530

    Dice Roller Update

--- dice-roller.js
+++ dice-roller.js
@@ -4,6 +4,12 @@ let maxDice = 10
 let minSidesOfDice = 0
 let maxSidesOfDice = 25
 
-
+//Part 1
+// Dice1
+let Dice1
+function randomDicePart1() {
+    Dice1 = minDice + Math.floor(Math.random() * (maxDice - minDice))
+    console.log(`Random number of Dice 1 is : ${Dice1}`);
+}
 
 
